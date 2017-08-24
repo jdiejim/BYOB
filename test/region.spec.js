cc09ac05b344b07d58c0fc95a281d4bec31061da
Juan Diego Jimenez <jdiejim@gmail.com>
2017-08-24 15:34:35 -0600

    Add no token test for delete region endpoint

--- test/region.spec.js
+++ test/region.spec.js
@@ -259,5 +259,15 @@ describe('API Region Routes', () => {
           done();
         });
     });
+
+    it('should return error if no token attached', (done) => {
+      chai.request(server)
+        .delete('/api/v1/region/1')
+        .end((err, res) => {
+          res.should.have.status(403);
+          res.body.error.should.equal('You must be authorized to hit this endpoint');
+          done();
+        });
+    });
   });
 });
