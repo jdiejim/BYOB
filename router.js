63cccc13360fba6cc66f0cc0bc6f102eee3c224a
Juan Diego Jimenez <jdiejim@gmail.com>
2017-08-24 14:20:46 -0600

    Add check auth admin middleware for region and industry endpoints

--- router.js
+++ router.js
@@ -3,25 +3,24 @@ const betas = require('./controllers/betasController');
 const industry = require('./controllers/industryController');
 const region = require('./controllers/regionController');
 const auth = require('./controllers/authController');
-const { checkAuth } = require('./utils/middleware');
+const { checkAuthAdmin } = require('./utils/middleware');
 
 const router = express.Router();
 
 // Auth
-// router.get('/authentication', auth.getToken);
 router.post('/authentication', auth.getToken);
 
 // Industry
-router.post('/industry', industry.create);
+router.post('/industry', checkAuthAdmin, industry.create);
 router.get('/industry', industry.index);
-router.put('/industry/:id', industry.update);
-router.delete('/industry/:id', industry.remove);
+router.put('/industry/:id', checkAuthAdmin, industry.update);
+router.delete('/industry/:id', checkAuthAdmin, industry.remove);
 
 // Regions
-router.post('/region', region.create);
+router.post('/region', checkAuthAdmin, region.create);
 router.get('/region', region.index);
-router.put('/region/:id', region.update);
-router.delete('/region/:id', region.remove);
+router.put('/region/:id', checkAuthAdmin, region.update);
+router.delete('/region/:id', checkAuthAdmin, region.remove);
 
 // Betas
 router.get('/betas', betas.index);
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,23 +1,20 @@
 const jwt = require('jsonwebtoken');
-const app = require('../server.js');
 
-exports.checkAuth = (req, res, next) => {
+exports.checkAuthAdmin = (req, res, next) => {
   const token = req.headers.token;
 
   if (!token) {
     return res.status(403).json({ error: 'You must be authorized to hit this endpoint.' });
   }
 
-  // try {
-  //   const decoded = jwt.verify(token, app.get('secretKey'));
-  //
-  //   if (decoded.admin) {
-  //     return next();
-  //   }
-  //
-  //   return res.status(403).json({ error: 'You must be an admin to hit this endpoint.' });
-  //
-  // }catch(err) {
-  //   return res.status(403).json({ error: 'Invalid token' });
-  // }
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    if (decoded.admin) {
+      return next();
+    }
+
+    return res.status(403).json({ error: 'You must be an admin to hit this endpoint.' });
+  } catch (err) {
+    return res.status(403).json({ error: 'Invalid token' });
+  }
 };
