9c90c045f4cce6f51aeaca44768441c71e79e6c1
Juan Diego Jimenez <jdiejim@gmail.com>
2017-08-24 18:34:13 -0600

    Add invalid token test for get betas/region endpoint

--- test/betas.spec.js
+++ test/betas.spec.js
@@ -303,5 +303,16 @@ describe('API Beta Routes', () => {
           done();
         });
     });
+
+    it('should return error if invalid token attached', (done) => {
+      chai.request(server)
+        .get('/api/v1/betas/region/1')
+        .set('Token', invalidToken)
+        .end((err, res) => {
+          res.should.have.status(403);
+          res.body.error.should.equal('Invalid token');
+          done();
+        });
+    });
   });
 });
