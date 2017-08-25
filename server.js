66391dc91751eca35ed4e56eb712ca2c12882de5
Juan Diego Jimenez <jdiejim@gmail.com>
2017-08-24 19:47:59 -0600

    Add webpack setup

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 /* eslint no-console: "off" */
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./router');
@@ -10,8 +11,10 @@ app.locals.title = 'BYOB';
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, './build')));
 app.use('/api/v1', router);
 
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '/build', 'index.html')));
 app.listen(app.get('port'), () => console.log(`${app.locals.title} is running on ${app.get('port')}`));
 
 module.exports = app;
