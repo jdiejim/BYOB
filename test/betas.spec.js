57bc194e3621cca954c5e04ceffe77728eac0e98
Juan Diego Jimenez <jdiejim@gmail.com>
2017-08-24 18:26:00 -0600

    Add test for get betas/industry endpoint

--- test/betas.spec.js
+++ test/betas.spec.js
@@ -189,4 +189,35 @@ describe('API Beta Routes', () => {
         });
     });
   });
+
+  describe('GET /betas/industry/:industry_id', () => {
+    it('should return all betas for a specified industry', (done) => {
+      chai.request(server)
+        .get('/api/v1/betas/industry/1')
+        .set('Token', normalToken)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.length.should.equal(7);
+          res.body[0].should.have.property('num_firms');
+          res.body[0].num_firms.should.equal(41);
+          res.body[0].should.have.property('average_unlevered_beta');
+          res.body[0].average_unlevered_beta.should.equal(0.910182);
+          res.body[0].should.have.property('average_levered_beta');
+          res.body[0].average_levered_beta.should.equal(1.363);
+          res.body[0].should.have.property('average_corr_market');
+          res.body[0].average_corr_market.should.equal(0.183748);
+          res.body[0].should.have.property('total_unlevered_beta');
+          res.body[0].total_unlevered_beta.should.equal(4.95343);
+          res.body[0].should.have.property('total_levered_beta');
+          res.body[0].total_levered_beta.should.equal(7.41777);
+          res.body[0].should.have.property('industry');
+          res.body[0].industry.should.equal('Advertising');
+          res.body[0].should.have.property('region');
+          res.body[0].region.should.equal('US');
+          done();
+        });
+    });
+  });
 });
