757337b068d0287c92ca6ec9390829c6b5e02dfd
Juan Diego Jimenez <jdiejim@gmail.com>
2017-08-26 09:44:23 -0600

    Fix delete test for industry endpoint

--- test/industry.spec.js
+++ test/industry.spec.js
@@ -220,15 +220,16 @@ describe('API Industry Routes', () => {
   });
 
   describe('DELETE /api/v1/industry/:id', () => {
-    it.skip('should delete industry', (done) => {
+    it('should delete an industry', (done) => {
       chai.request(server)
         .get('/api/v1/industry/')
         .set('Token', adminToken)
         .end((err, res) => {
+          const id = res.body[0].id;
           res.should.have.status(200);
           res.body[0].industry.should.equal('Advertising');
           chai.request(server)
-            .delete('/api/v1/industry/1')
+            .delete(`/api/v1/industry/${id}`)
             .set('Token', adminToken)
             .end((error, response) => {
               response.should.have.status(200);
@@ -239,26 +240,26 @@ describe('API Industry Routes', () => {
                 .set('Token', adminToken)
                 .end((e, r) => {
                   r.should.have.status(200);
-                  expect(r.body.find(el => el.industry === 'Advertising')).to.equal(undefined);
+                  r.body[0].industry.should.not.equal('Advertising');
                   done();
                 });
             });
         });
     });
 
-    it.skip('should return not found if industry does not exists', (done) => {
+    it('should return not found if industry does not exists', (done) => {
       chai.request(server)
-        .delete('/api/v1/industry/100')
+        .delete('/api/v1/industry/0')
         .set('Token', adminToken)
         .send({ name: 'Sports' })
         .end((err, res) => {
           res.should.have.status(404);
-          res.body.error.should.equal('Not Found');
+          res.body.error.should.equal('Industry not Found');
           done();
         });
     });
 
-    it.skip('should return error if no token attached', (done) => {
+    it('should return error if no token attached', (done) => {
       chai.request(server)
         .delete('/api/v1/industry/1')
         .end((err, res) => {
@@ -268,7 +269,7 @@ describe('API Industry Routes', () => {
         });
     });
 
-    it.skip('should return error if invalid token attached', (done) => {
+    it('should return error if invalid token attached', (done) => {
       chai.request(server)
         .delete('/api/v1/industry/1')
         .set('Token', invalidToken)
@@ -279,7 +280,7 @@ describe('API Industry Routes', () => {
         });
     });
 
-    it.skip('should return error if non-admin token attached', (done) => {
+    it('should return error if non-admin token attached', (done) => {
       chai.request(server)
         .delete('/api/v1/industry/1')
         .set('Token', normalToken)
